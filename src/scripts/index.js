6df50ee3b674470ec3b369e4078eaca2e1c4ed9e
Hiago Rocha <hiagorocha16@outlook.com>
2022-09-08 19:00:18 -0300

    feat: add js login

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,6 +1,44 @@
-const inputEmail = document.getElementById("email")
-const inputSenha = document.getElementById("senha")
+import { ApiRequests } from "./models/api.js"
 
-function VerificandoLogin(email,senha){
-    
+const btnEntendi = document.querySelector(".modal-btn")
+btnEntendi.addEventListener("click", (event) => {
+    event.preventDefault()
+    document.querySelector(".modal-wrapper").classList.toggle("show-modal")
+})
+
+class LoginPage {
+    static renderLoginPage(){
+        const token = localStorage.getItem("@kenzieStore:token")
+
+        if(token){
+            window.location.assign("src/pages/home.html")
+        }
+
+        const emailInput = document.getElementById("email")
+        const senhaInput = document.getElementById("senha")
+        const btnLogin = document.querySelector(".btn-logar")
+
+        btnLogin.addEventListener("click", (event) => {
+            event.preventDefault()
+
+            const data = {
+                email: emailInput.value,
+                password: senhaInput.value
+            }
+
+            ApiRequests.login(data)
+            window.location.assign("src/pages/home.html")
+        })
+    }
+    static handleSignup() {
+        const handleSignupBtn = document.querySelector(".btn-registro")
+
+        handleSignupBtn.addEventListener("click", (event) => {
+            event.preventDefault()
+            window.location.assign("src/pages/cadastro.html")
+        })
+    }
 }
+
+LoginPage.renderLoginPage()
+LoginPage.handleSignup()
